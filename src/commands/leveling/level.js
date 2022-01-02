89f44eea3e0d6e5617197c7a8d94d161b6e6173f
TareGit <Tareebelo4@gmail.com>
2022-01-01 19:19:11 -0500

    transparency on rank card

--- src/commands/leveling/level.js
+++ src/commands/leveling/level.js
@@ -93,7 +93,7 @@ module.exports = {
                     display: flex;
                     justify-content: center;
                     align-items: center;
-                    /*background: transparent;*/
+                    background: transparent;
                 }
         
                 .user-profile {
